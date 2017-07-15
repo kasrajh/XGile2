eaaa82b00c22642b99dfa6cada47ea822bb3929a
Student <Student@A5-PC>
2017-07-15 12:42:01 +0430

    Add project files.

---
+++ XGile2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace XGile2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ XGile2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="XGile2.MvcApplication" Language="C#" %>
